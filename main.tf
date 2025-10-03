1a24ca6d6203d82ece2e79e6b791694845828c60
Tanisha-221 <tanishajindal223@gmail.com>
2025-10-03 16:10:54 +0530

    added storage account block in main.tf

--- main.tf
+++ main.tf
@@ -67,9 +67,19 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
-resource "azurerm_virtual_network" "AVn" {
-  name                = "TA-network"
-  address_space       = ["10.0.1.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "tstoacc542245"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
 }
