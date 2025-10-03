ec74616f0cc364375befd66f5060f36853d5fcb7
Tanisha-221 <tanishajindal223@gmail.com>
2025-10-03 16:03:26 +0530

    modified the main.tf

--- main.tf
+++ main.tf
@@ -66,3 +66,10 @@ resource "azurerm_virtual_machine" "main" {
     environment = "staging"
   }
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "TA-network"
+  address_space       = ["10.0.1.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
